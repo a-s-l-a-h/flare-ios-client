b4719fa5db73c91b2d49bb0c7337a6dd1412d490
a-s-l-a-h <61a73s6dm@gmail.com>
2026-08-28 11:35:57 +0530

    .

--- Package.swift
+++ Package.swift
@@ -11,13 +11,16 @@ let package = Package(
         .library(name: "FlareClientExtensionsBuiltin", targets: ["FlareClientExtensionsBuiltin"])
     ],
     dependencies: [
+        // Pin VGSL to a stable version prior to the 7.25.4 regression
+        .package(url: "https://github.com/yandex/vgsl.git", exact: "7.24.0"),
         .package(url: "https://github.com/divkit/divkit-ios.git", exact: "32.52.0")
     ],
     targets: [
         .target(
             name: "FlareClient",
             dependencies: [
-                .product(name: "DivKit", package: "divkit-ios")
+                .product(name: "DivKit", package: "divkit-ios"),
+                .product(name: "VGSL", package: "vgsl")
             ],
             path: "FlareClient",
             sources: ["Sources"],
