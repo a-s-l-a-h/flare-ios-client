27c49a7626f23e0101f6e72752aaa0bcf24b0917
a-s-l-a-h <61a73s6dm@gmail.com>
2026-08-28 11:23:00 +0530

    Update DivKit to 32.52.0 and setup XcodeGen CI build

--- Package.swift
+++ Package.swift
@@ -11,26 +11,25 @@ let package = Package(
         .library(name: "FlareClientExtensionsBuiltin", targets: ["FlareClientExtensionsBuiltin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/divkit/divkit-ios.git", exact: "30.1.0")
+        .package(url: "https://github.com/divkit/divkit-ios.git", exact: "32.52.0")
     ],
     targets: [
-            .target(
-                name: "FlareClient",
-                dependencies: [
-                    .product(name: "DivKit", package: "divkit-ios"),
-                    .product(name: "LayoutKit", package: "divkit-ios")
-                ],
-                path: "FlareClient",
-                sources: ["Sources"],
-                resources: [
-                    .process("Resources")
-                ]
-            ),
-            .target(
-                name: "FlareClientExtensionsBuiltin",
-                dependencies: ["FlareClient"],
-                path: "FlareClientExtensionsBuiltin",
-                sources: ["Sources"]
-            )
-        ]
+        .target(
+            name: "FlareClient",
+            dependencies: [
+                .product(name: "DivKit", package: "divkit-ios")
+            ],
+            path: "FlareClient",
+            sources: ["Sources"],
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .target(
+            name: "FlareClientExtensionsBuiltin",
+            dependencies: ["FlareClient"],
+            path: "FlareClientExtensionsBuiltin",
+            sources: ["Sources"]
+        )
+    ]
 )
--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, '14.0'
-use_frameworks!
-
-target 'FlareApp' do
-  pod 'DivKit', '30.1.0'
-  pod 'LayoutKit', '30.1.0'
-end
